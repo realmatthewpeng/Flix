161128cd39d11b8f8ab52320fe4e81fd2c9f74ec
Matthew Peng <MP@Matts-Mac.local>
2021-09-21 23:15:47 -0700

    Added bonus user stories

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -29,7 +29,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         layout.itemSize = CGSize(width: width, height: width * 1.5)
 
         // TODO: Get the array of movies
-        let url = URL(string: "https://api.themoviedb.org/3/movie/299534/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -68,14 +68,20 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return movies.count
     }
     
-    /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
-
+        
+        // Find selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass selected movie to details view contoller
+        let detailsViewController = segue.destination as! MovieGridDetailsViewController
+        detailsViewController.movie = movie
+        
+        }
 }
--- README.md
+++ README.md
@@ -3,7 +3,22 @@
 Flix is an app that allows users to browse movies from the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
 
 ---
+## Flix Part 2
 
+### User Stories
+
+#### REQUIRED (10pts)
+- [X] (5pts) User can tap a cell to see more details about a particular movie.
+- [X] (5pts) User can tap a tab bar button to view a grid layout of Movie Posters using a CollectionView.
+
+#### BONUS
+- [X] (2pts) User can tap a poster in the collection view to see a detail screen of that movie.
+- [ ] (2pts) In the detail view, when the user taps the poster, a new screen is presented modally where they can view the trailer.
+
+### App Walkthrough GIF
+
+### Notes
+I did not have any issues with this week's assignent.
 ## Flix Part 1
 
 ### User Stories
